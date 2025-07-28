7641eeaba5b180590492ec4fb344406510cb86b5
Paulo Henrique Duarte <multidesk.io@gmail.com>
2025-07-28 14:36:21 +0000

    tanto o header quanto o menu estão rolando

--- src/app/manager/layout.tsx
+++ src/app/manager/layout.tsx
@@ -77,7 +77,7 @@ export default function ManagerLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <ManagerSidebar />
       <div className="flex flex-col">
         <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
@@ -176,7 +176,7 @@ export default function ManagerLayout({
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 overflow-auto">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 overflow-y-auto">
             {children}
         </main>
       </div>
