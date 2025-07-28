6bb3ea3218e001fdc78276d6580dd7bd9f48e9e0
Paulo Henrique Duarte <multidesk.io@gmail.com>
2025-07-28 19:59:34 +0000

    CREDENCIAL ERRADA DE NOVO

---
+++ drizzle/0001_puzzling_swordsman.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "users" ALTER COLUMN "password" SET DATA TYPE varchar(255);--> statement-breakpoint
+ALTER TABLE "church_profiles" ALTER COLUMN "treasurer_cpf" SET DATA TYPE varchar(14);
