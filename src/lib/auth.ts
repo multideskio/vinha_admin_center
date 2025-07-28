e7136fb219a2663253538ea4fb5659712a3be7bb
Paulo Henrique Duarte <multidesk.io@gmail.com>
2025-07-28 21:07:12 +0000

    no navegador normail deu certo, mas aqui não quer manter o login, ele ex

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,6 @@ import { Lucia } from "lucia";
 import { db } from "@/db/drizzle";
 import { sessions, users, userRoleEnum } from "@/db/schema";
 import { cookies } from "next/headers";
-import { cache } from "react";
 import type { Session, User } from "lucia";
 
 
@@ -28,31 +27,30 @@ export const lucia = new Lucia(adapter, {
     }
 });
 
-export const validateRequest = cache(
-	async (): Promise<{ user: User; session: Session } | { user: null; session: null }> => {
-		const sessionId = (await cookies()).get(lucia.sessionCookieName)?.value ?? null;
-		if (!sessionId) {
-			return {
-				user: null,
-				session: null
-			};
-		}
+export const validateRequest = async (): Promise<{ user: User; session: Session } | { user: null; session: null }> => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+    if (!sessionId) {
+        return {
+            user: null,
+            session: null
+        };
+    }
 
-		const result = await lucia.validateSession(sessionId);
-		// next.js throws when you attempt to set cookie when rendering page
-		try {
-			if (result.session && result.session.fresh) {
-				const sessionCookie = lucia.createSessionCookie(result.session.id);
-				(await cookies()).set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-			}
-			if (!result.session) {
-				const sessionCookie = lucia.createBlankSessionCookie();
-				(await cookies()).set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-			}
-		} catch {}
-		return result;
-	}
-);
+    const result = await lucia.validateSession(sessionId);
+    // next.js throws when you attempt to set cookie when rendering page
+    try {
+        if (result.session && result.session.fresh) {
+            const sessionCookie = lucia.createSessionCookie(result.session.id);
+            cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+        }
+        if (!result.session) {
+            const sessionCookie = lucia.createBlankSessionCookie();
+            cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+        }
+    } catch {}
+    return result;
+};
+
 
 // IMPORTANT!
 declare module "lucia" {
