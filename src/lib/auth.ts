f705243965c541d4668d15ac3f137d58150f921d
Paulo Henrique Duarte <multidesk.io@gmail.com>
2025-07-29 05:43:05 +0000

    auth.ts:31  Server   Error: Route "/admin/configuracoes/mensagens" used

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,4 @@
 
-
 import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 import { Lucia, TimeSpan } from "lucia";
 import { db } from "@/db/drizzle";
@@ -27,29 +26,31 @@ export const lucia = new Lucia(adapter, {
     }
 });
 
-export const validateRequest = async (): Promise<{ user: User; session: Session } | { user: null; session: null }> => {
-	const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
-	if (!sessionId) {
-		return {
-			user: null,
-			session: null
-		};
-	}
+export const validateRequest = cache(
+	async (): Promise<{ user: User; session: Session } | { user: null; session: null }> => {
+		const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+		if (!sessionId) {
+			return {
+				user: null,
+				session: null
+			};
+		}
 
-	const result = await lucia.validateSession(sessionId);
-	// next.js throws when you attempt to set cookie when rendering page
-	try {
-		if (result.session && result.session.fresh) {
-			const sessionCookie = lucia.createSessionCookie(result.session.id);
-			cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-		}
-		if (!result.session) {
-			const sessionCookie = lucia.createBlankSessionCookie();
-			cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-		}
-	} catch {}
-	return result;
-};
+		const result = await lucia.validateSession(sessionId);
+		// next.js throws when you attempt to set cookie when rendering page
+		try {
+			if (result.session && result.session.fresh) {
+				const sessionCookie = lucia.createSessionCookie(result.session.id);
+				cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+			}
+			if (!result.session) {
+				const sessionCookie = lucia.createBlankSessionCookie();
+				cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+			}
+		} catch {}
+		return result;
+	}
+);
 
 
 // IMPORTANT!
